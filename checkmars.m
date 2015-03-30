0f70cadbf4eec4a675cf884a0340cc6fb3e426e8
Charlotte EL <shiningsilver@gmail.com>
2015-03-30 02:14:56 +0100

    Create checkmars.m

---
+++ checkmars.m
@@ -0,0 +1,14 @@
+function arrvals = checkmars(array);
+% uses a, e, theta values from results array to check for arrival at Mars
+% orbit
+for i=1:length(array)
+    a=array(i,2);
+    e=array(i,3);
+    th=array(i,1);
+    radius=(a*(1-(e^2)))/(1+(e*cos(th)));
+    if radius >= 227939100
+        disp(array(i,:))
+        break
+    end
+end
+end
