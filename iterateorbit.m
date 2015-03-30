7723163acea4c780442fd3b5845a3d55f7d28a94
Charlotte EL <shiningsilver@gmail.com>
2015-03-30 01:08:16 +0100

    Create iterateorbit.m

---
+++ iterateorbit.m
@@ -0,0 +1,9 @@
+x0=[154843527.6 0.033877 0 0];
+for i=1:314
+    finalth=i*0.01;
+    thspan=[finalth-0.01,finalth];
+    [theta,allx]=ode45(@lagrange,thspan,x0);
+    dlmwrite('xvalsbb.txt',allx,'-append','precision',10);
+    dlmwrite('thvalsbb.txt',theta,'-append','precision',10);
+    x0=[allx(end,1),allx(end,2),allx(end,3),allx(end,4)];
+end
